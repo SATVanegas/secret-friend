5ac2a17f2fd124592c03d60e39b5266090c05074
SATvanegas <svanegasv@unal.edu.co>
2025-01-31 09:49:28 -0500

    fuction addFriend

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let friends = [];
+
+
+function addFriend() {
+    if (document.getElementById('friend').value == '') {
+        alert('Por favor, inserte un nombre');
+   
+    }else{
+        friends.push(document.getElementById('friend').value);
+    }
+}
